431f87f7e0743abd2f4423b2b48ead2cbd61b143
harisjpg <harmaulana18@gmail.com>
2024-05-15 13:29:49 +0700

    penambahan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,12 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Menu;
+use App\Models\RelationLob;
+use App\Models\RelationStatus;
+use App\Models\RelationType;
 use App\Models\Role;
 use App\Models\RoleAccessMenu;
+use App\Models\Salutation;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -81,5 +85,76 @@ class DatabaseSeeder extends Seeder
                 'role_id' => $user->id
             ]
         );
+
+        // Create Relation Lob
+        RelationLob::create(
+            [
+                'RELATION_LOB_NAME' => 'Pertanian Tanaman, Peternakan, Perburuan dan Kegiatan ybdi',
+                'RELATION_LOB_DESC' => NULL
+            ]
+        );
+
+        RelationLob::create(
+            [
+                'RELATION_LOB_NAME' => 'Kehutanan dan Penebangan Kayu',
+                'RELATION_LOB_DESC' => NULL
+            ]
+        );
+
+        RelationLob::create(
+            [
+                'RELATION_LOB_NAME' => 'Asuransi',
+                'RELATION_LOB_DESC' => NULL
+            ]
+        );
+
+
+        // create relation type
+        RelationType::create(
+            [
+                'RELATION_TYPE_NAME' => 'Insurance',
+                'RELATION_TYPE_DESCRIPTION' => NULL
+            ]
+        );
+        RelationType::create(
+            [
+                'RELATION_TYPE_NAME' => 'Agent',
+                'RELATION_TYPE_DESCRIPTION' => NULL
+            ]
+        );
+        RelationType::create(
+            [
+                'RELATION_TYPE_NAME' => 'Broker',
+                'RELATION_TYPE_DESCRIPTION' => NULL
+            ]
+        );
+
+        // created salutation
+        Salutation::create([
+            'salutation_name' => 'CV',
+            'salutation_desc' => NULL,
+            'salutation_status_id' => '1'
+        ]);
+
+        Salutation::create([
+            'salutation_name' => 'PT',
+            'salutation_desc' => NULL,
+            'salutation_status_id' => '1'
+        ]);
+
+        Salutation::create([
+            'salutation_name' => 'PD',
+            'salutation_desc' => NULL,
+            'salutation_status_id' => '1'
+        ]);
+
+        // created ralation status
+        RelationStatus::create([
+            'relation_status_name' => 'Corporate',
+        ]);
+
+        RelationStatus::create([
+            'relation_status_name' => 'Individu',
+        ]);
     }
 }
