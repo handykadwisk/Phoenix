4f7e315de3c9cc4f0658f6d586dbba47fdbb8baa
handykadwisk <handykadwisk@gmail.com>
2024-09-13 10:21:37 +0700

    revisi

--- database/migrations/0001_01_01_000000_create_t_users_table.php
+++ database/migrations/0001_01_01_000000_create_t_users_table.php
@@ -13,16 +13,12 @@ return new class extends Migration
     {
         Schema::create('t_user', function (Blueprint $table) {
             $table->id();
-            $table->smallInteger('employee_id')->nullable();
-            $table->smallInteger('relation_status_id')->nullable();
-            $table->string('user_login')->unique()->nullable();
+            $table->smallInteger('person_id')->nullable();
             $table->string('name');
-            $table->foreignId('user_type_id')->nullable();
-            $table->string('password');
-            // $table->string('email')->unique()->nullable();
-            // $table->foreignId('role_id')->nullable();
-            $table->smallInteger('user_status')->default(1);
+            $table->string('email')->unique();
+            $table->foreignId('role_id');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
