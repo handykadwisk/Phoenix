e457ff0f4a866008027643ed1bed4207a5cd7699
Apep <apep.susnendar@gmail.com>
2024-09-13 14:41:45 +0700

    fixed routing att

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,13 +1,14 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, usePage } from "@inertiajs/react";
 import { PageProps } from "@/types";
-import PhoenixComponent from "@/Utility/PhoenixComponent";
+import Button from "@/Components/Button/Button";
 
 export default function Dashboard({ auth, language }: any) {
+
     // props language dikirim langsung dari middleware Language yang diinject langsung melalui Inertia. cek file Language.php di folder Middleware
     // kemudian ambil array pertama dari language
     // selanjutnya, tinggal panggil di tiap string yang mau diambil dari file language. contoh: lang.policy --ada di bawah contohnya--
-    const lang: any = language[0];
+    const lang: any = language[0]
 
     const stats = [
         { name: lang.policy, stat: "0" },
@@ -19,15 +20,37 @@ export default function Dashboard({ auth, language }: any) {
         <AuthenticatedLayout user={auth.user} header={lang.dashboard}>
             <Head title={lang.dashboard} />
 
-            {/* <PhoenixComponent /> */}
             <div>
-                <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
+                    <div
+                        // key={item.name}
+                        className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+                    >
+                        <dt className="truncate text-sm font-medium text-gray-500">
+                            {/* {item.name} */}
+                            {"Attendance"}
+                        </dt>
+                        <dd className="mt-3 text-sm font-semibold tracking-tight text-gray-900">
+                            {"Click "}
+                            <a
+                                className="focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-white bg-red-600 rounded-md"
+                                href="attendance"
+                                arial-label="your link text"
+                                title="your link text"
+                                target="_blank"
+                            >
+                                here
+                            </a>
+
+                            {" for attendance"}
+                        </dd>
+                    </div>
                     {stats.map((item) => (
                         <div
                             key={item.name}
                             className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
                         >
-                            <dt className="truncate text-sm font-medium text-gray-500 cls_can_attach_process cls_can_attach_process">
+                            <dt className="truncate text-sm font-medium text-gray-500">
                                 {item.name}
                             </dt>
                             <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
