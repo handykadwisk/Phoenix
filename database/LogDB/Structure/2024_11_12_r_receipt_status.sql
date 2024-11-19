5608576c444ec165977ca12499336b12081e6215
Fadhlan Agung <fadhlan.priyatna@killianindonesia.com>
2024-11-19 16:07:24 +0700

    update r_receipt_status

--- database/LogDB/Structure/2024_11_12_r_receipt_status.sql
+++ database/LogDB/Structure/2024_11_12_r_receipt_status.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.33 (64 bit)
 MySQL - 8.0.31 : Database - phoenix
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,7 +13,7 @@ MySQL - 8.0.31 : Database - phoenix
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`phoenix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`phoenix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 /*Table structure for table `r_receipt_status` */
 
@@ -20,7 +21,7 @@ DROP TABLE IF EXISTS `r_receipt_status`;
 
 CREATE TABLE `r_receipt_status` (
   `RECEIPT_STATUS_ID` int NOT NULL AUTO_INCREMENT,
-  `RECEIPT_STATUS_NAME` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `RECEIPT_STATUS_NAME` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`RECEIPT_STATUS_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
