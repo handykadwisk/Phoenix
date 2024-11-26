430900eb4474c620b9aa68a849d55e6adb4aae72
Pian <adityapian6@gmail.com>
2024-11-26 17:04:36 +0700

    collation in tabel maternity

--- database/LogDB/structure/20241126_tabel_for_maternity_by_grade.sql
+++ database/LogDB/structure/20241126_tabel_for_maternity_by_grade.sql
@@ -21,7 +21,7 @@ CREATE TABLE `m_grade_maternity_limit` (
   `GRADE_ID` int DEFAULT NULL,
   `MATERNITY_LIMIT_ID` int DEFAULT NULL,
   PRIMARY KEY (`GRADE_MATERNITY_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `m_grade_maternity_limit` */
 
@@ -35,6 +35,27 @@ insert  into `m_grade_maternity_limit`(`GRADE_MATERNITY_ID`,`GRADE_ID`,`MATERNIT
 (7,7,4),
 (8,8,5),
 (9,9,6);
+
+/*Table structure for table `r_maternity_limit` */
+
+DROP TABLE IF EXISTS `r_maternity_limit`;
+
+CREATE TABLE `r_maternity_limit` (
+  `MATERNITY_LIMIT_ID` int NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `MATERNITY_LIMIT_AMOUNT` decimal(20,2) DEFAULT NULL,
+  PRIMARY KEY (`MATERNITY_LIMIT_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `r_maternity_limit` */
+
+insert  into `r_maternity_limit`(`MATERNITY_LIMIT_ID`,`DESCRIPTION`,`MATERNITY_LIMIT_AMOUNT`) values 
+(1,'Untuk Golongan A',15000000.00),
+(2,'Untuk Golongan B',12500000.00),
+(3,'Untuk Golongan C',10000000.00),
+(4,'Untuk Golongan D',7500000.00),
+(5,'Untuk Golongan E',5000000.00),
+(6,'Untuk Golongan F',2500000.00);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
