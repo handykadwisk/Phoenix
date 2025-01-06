b805e8c0c51dec760f51710cd7d45d5499537f14
Fadhlan Agung <fadhlan.priyatna@killianindonesia.com>
2025-01-06 08:15:29 +0700

    Update index AG Grid

--- resources/js/Components/AgGrid/index.tsx
+++ resources/js/Components/AgGrid/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { PropsWithChildren, useEffect, useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
@@ -24,6 +24,9 @@ export default function AGGrid({
     rowSelection,
     onSelectionChanged,
     suppressRowClickSelection,
+    buttonExcelExport,
+    suppressCsvExport,
+    noRowsOverlayComponent,
 }: PropsWithChildren<{
     colDefs: any;
     url: string;
@@ -36,8 +39,11 @@ export default function AGGrid({
     addButtonModalState: CallableFunction | undefined;
     rowHeight?: number | undefined;
     rowSelection?: any;
-    onSelectionChanged?: CallableFunction | any;
+    onSelectionChanged?: any;
     suppressRowClickSelection?: boolean;
+    buttonExcelExport?: boolean;
+    suppressCsvExport?: boolean;
+    noRowsOverlayComponent?: boolean;
 }>) {
     const gridRef = useRef<AgGridReact>(null);
     const getServerSideDatasource = (): IServerSideDatasource => {
@@ -77,8 +83,8 @@ export default function AGGrid({
                     urlNew = `${url}?`;
                 }
 
-                console.log("Filter model from AG Grid:", filterModel);
-                console.log("Filter params sent to API:", filterParams);
+                // console.log("Filter model from AG Grid:", filterModel);
+                // console.log("Filter params sent to API:", filterParams);
 
                 axios
                     .get(
@@ -93,28 +99,50 @@ export default function AGGrid({
                             rowData: res.data.data,
                             rowCount: res.data.total,
                         });
+                        if (res.data.data.length === 0) {
+                            params.api.showNoRowsOverlay();
+                        } else {
+                            params.api.hideOverlay();
+                        }
                     })
                     .catch((err) => console.log(err));
             },
         };
     };
 
-    const handleRowSelectedChange = (params: any) => {
-        // console.log("Selected Row", params.api.getSelectedRows());
-        // params.api.deselectAll();
-        const dataSelected = params.api.getSelectedRows();
-        onSelectionChanged(dataSelected);
-    };
-
     const onGridReady = (params: GridReadyEvent<any, any>) => {
         var dataSource = getServerSideDatasource();
         params.api!.setGridOption("serverSideDatasource", dataSource);
-        // params.api.deselectAll();
-        // params.api!.sizeColumnsToFit()
     };
 
     const doubleClicked = (params: any) => {
         doubleClickEvent(params.data);
+    };
+
+    const handleRowSelectedChange = (params: any) => {
+        const dataSelected = params.api.getSelectedRows();
+        // console.log("Selected Row", dataSelected);
+        onSelectionChanged(dataSelected);
+    };
+
+    const handleButtonExcelExport = useCallback(() => {
+        if (gridRef.current?.api) {
+            gridRef.current.api.exportDataAsExcel({
+                fileName: "Receipt",
+            });
+        }
+    }, []);
+
+    const customNoRowsOverlayComponent = () => {
+        return (
+            <>
+                {noRowsOverlayComponent && (
+                    <div className="text-slate-500 font-bold text-xl">
+                        No Data Available
+                    </div>
+                )}
+            </>
+        );
     };
 
     useEffect(() => {
@@ -122,48 +150,62 @@ export default function AGGrid({
             gridRef.current!.api!.refreshServerSide({ purge: true });
             gridRef.current!.api!.deselectAll();
         }
-    }, [triggeringRefreshData, gridRef]);
+    }, [triggeringRefreshData]);
+
+    console.log("Trigger Refresh Data", triggeringRefreshData);
 
     return (
         // <div className="rounded-md shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-2.5">
-        <div className="flex flex-row items-center h-[100%]">
-            {addButtonLabel && (
-                <div className="w-96">
-                    <Button
-                        className="text-sm w-full lg:w-1/2 font-semibold px-3 py-1.5 mb-4 md:col-span-2 text-white"
-                        onClick={() => addButtonModalState()}
-                    >
-                        {addButtonLabel}
-                    </Button>
-                </div>
+        <>
+            {buttonExcelExport && (
+                <button
+                    onClick={handleButtonExcelExport}
+                    className="border border-gray-200 hover:bg-gray-200 font-bold text-sm px-3 py-2 mb-5 rounded-md"
+                >
+                    Export to Excel
+                </button>
             )}
-            <div className="w-full h-[100%] overflow-x-auto ag-grid-table custom-scrollbar overflow-visible ag-theme-quartz">
-                <AgGridReact
-                    ref={gridRef}
-                    columnDefs={colDefs}
-                    getRowStyle={(params: any) => {
-                        if (params.node.rowIndex % 2 !== 0) {
-                            return {
-                                background: "#fafafb",
-                            };
-                        }
-                    }}
-                    suppressServerSideFullWidthLoadingRow={true}
-                    pagination={true}
-                    paginationPageSize={10}
-                    // paginationAutoPageSize={true}
-                    cacheBlockSize={10}
-                    paginationPageSizeSelector={[1, 10, 25, 50, 100]}
-                    onGridReady={onGridReady}
-                    rowModelType="serverSide"
-                    onRowDoubleClicked={doubleClicked}
-                    rowHeight={rowHeight}
-                    rowSelection={rowSelection}
-                    onSelectionChanged={handleRowSelectedChange}
-                    suppressRowClickSelection={suppressRowClickSelection}
-                />
+            <div className="flex flex-row items-center h-[100%]">
+                {addButtonLabel && (
+                    <div className="w-96">
+                        <Button
+                            className="text-sm w-full lg:w-1/2 font-semibold px-3 py-1.5 mb-4 md:col-span-2 text-white"
+                            onClick={() => addButtonModalState()}
+                        >
+                            {addButtonLabel}
+                        </Button>
+                    </div>
+                )}
+                <div className="w-full h-[100%] overflow-x-auto ag-grid-table custom-scrollbar overflow-visible ag-theme-quartz">
+                    <AgGridReact
+                        ref={gridRef}
+                        columnDefs={colDefs}
+                        getRowStyle={(params: any) => {
+                            if (params.node.rowIndex % 2 !== 0) {
+                                return {
+                                    background: "#fafafb",
+                                };
+                            }
+                        }}
+                        suppressServerSideFullWidthLoadingRow={true}
+                        pagination={true}
+                        paginationPageSize={10}
+                        // paginationAutoPageSize={true}
+                        cacheBlockSize={10}
+                        paginationPageSizeSelector={[1, 10, 25, 50, 100]}
+                        onGridReady={onGridReady}
+                        rowModelType="serverSide"
+                        onRowDoubleClicked={doubleClicked}
+                        rowHeight={rowHeight}
+                        rowSelection={rowSelection}
+                        onSelectionChanged={handleRowSelectedChange}
+                        suppressRowClickSelection={suppressRowClickSelection}
+                        suppressCsvExport={suppressCsvExport}
+                        noRowsOverlayComponent={customNoRowsOverlayComponent}
+                    />
+                </div>
             </div>
-        </div>
+        </>
         // </div>
     );
 }
