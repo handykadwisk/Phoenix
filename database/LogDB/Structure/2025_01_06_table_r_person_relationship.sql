bef73a63d753d3c63d24a5f28c1b4bdb501389f0
harisjpg <harmaulana18@gmail.com>
2025-01-07 08:53:36 +0700

    r_person_relationship

--- database/LogDB/Structure/2025_01_06_table_r_person_relationship.sql
+++ database/LogDB/Structure/2025_01_06_table_r_person_relationship.sql
@@ -19,8 +19,29 @@ DROP TABLE IF EXISTS `r_person_relationship`;
 CREATE TABLE `r_person_relationship` (
   `PERSON_RELATIONSHIP_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `PERSON_RELATIONSHIP_NAME` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PERSON_RELATIONSHIP_IS_FAMILY_MEMBER` bigint(1) DEFAULT '0' COMMENT '0 = No, 1 = Yes',
   PRIMARY KEY (`PERSON_RELATIONSHIP_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `r_person_relationship` */
+
+insert  into `r_person_relationship`(`PERSON_RELATIONSHIP_ID`,`PERSON_RELATIONSHIP_NAME`,`PERSON_RELATIONSHIP_IS_FAMILY_MEMBER`) values 
+(1,'Aunty',0),
+(2,'Brother',1),
+(3,'Cousin',0),
+(4,'Father',1),
+(5,'Grandfather',0),
+(6,'Grandmother',0),
+(7,'Sister',1),
+(8,'Stepdad',0),
+(9,'Stepmum',0),
+(10,'Uncle',0),
+(11,'Friend',0),
+(12,'Wife',1),
+(13,'Husband',1),
+(14,'Mother',1),
+(15,'Children',1),
+(16,'Others',0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
