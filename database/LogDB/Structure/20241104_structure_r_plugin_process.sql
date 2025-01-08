635956a00d9dc041bc5f7bae44aa8a44df413cfd
harisjpg <harmaulana18@gmail.com>
2025-01-08 11:38:59 +0700

    perubahan table r_plugin_process

--- database/LogDB/Structure/20241104_structure_r_plugin_process.sql
+++ database/LogDB/Structure/20241104_structure_r_plugin_process.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Trial v13.1.9 (64 bit)
-MySQL - 8.2.0 : Database - phoenix
+MySQL - 5.7.36 : Database - phoenix
 *********************************************************************
 */
 
@@ -17,14 +17,21 @@ MySQL - 8.2.0 : Database - phoenix
 DROP TABLE IF EXISTS `r_plugin_process`;
 
 CREATE TABLE `r_plugin_process` (
-  `PLUGIN_PROCESS_ID` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `PLUGIN_PROCESS_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `PLUG_PROCESS_CLASS` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `PLUGIN_PROCESS_SHOW_CONTEXT_MENU` smallint DEFAULT NULL,
-  `PLUGIN_PROCESS_SHOW_OBJECT` smallint DEFAULT NULL,
-  `PLUGIN_PROCESS_DESCRIPTION` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `PLUGIN_PROCESS_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `PLUGIN_PROCESS_NAME` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PLUG_PROCESS_CLASS` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PLUGIN_PROCESS_SHOW_CONTEXT_MENU` smallint(6) DEFAULT NULL,
+  `PLUGIN_PROCESS_SHOW_OBJECT` smallint(6) DEFAULT NULL,
+  `PLUGIN_PROCESS_DESCRIPTION` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`PLUGIN_PROCESS_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `r_plugin_process` */
+
+insert  into `r_plugin_process`(`PLUGIN_PROCESS_ID`,`PLUGIN_PROCESS_NAME`,`PLUG_PROCESS_CLASS`,`PLUGIN_PROCESS_SHOW_CONTEXT_MENU`,`PLUGIN_PROCESS_SHOW_OBJECT`,`PLUGIN_PROCESS_DESCRIPTION`) values 
+(1,'Chat','cls_attach_chat',1,1,NULL),
+(2,'Task','cls_attach_task',0,1,NULL),
+(5,'Reminder','cls_attach_reminder',0,1,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
