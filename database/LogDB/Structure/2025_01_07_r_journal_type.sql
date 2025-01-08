1ada6e78979b9dd0aca1e294859601204410a725
Fadhlan Agung <fadhlan.priyatna@killianindonesia.com>
2025-01-08 09:11:45 +0700

    take out migration

---
+++ database/LogDB/Structure/2025_01_07_r_journal_type.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 8.0.31 : Database - phoenix
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `r_journal_type` */
+
+DROP TABLE IF EXISTS `r_journal_type`;
+
+CREATE TABLE `r_journal_type` (
+  `JOURNAL_TYPE_ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `JOURNAL_TYPE_CODE` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `JOURNAL_TYPE_DESC` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`JOURNAL_TYPE_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `r_journal_type` */
+
+insert  into `r_journal_type`(`JOURNAL_TYPE_ID`,`JOURNAL_TYPE_CODE`,`JOURNAL_TYPE_DESC`) values (1,'GDJ','General Disbursement Journal'),(2,'GJ','General Journal'),(3,'GRJ','General Receipt Journal'),(4,'JD','Journal Production'),(5,'PCJ','Petty Cash Journal'),(6,'PDJ','Premium Disbursement Journal'),(7,'PRJ','Premium Receipt Journal');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
