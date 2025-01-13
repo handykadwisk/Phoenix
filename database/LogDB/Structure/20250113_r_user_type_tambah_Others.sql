b2939421692fccf36eaaca60ea7cc27b2dc90aa1
handykadwisk <handykadwisk@gmail.com>
2025-01-13 15:09:46 +0700

    user type

---
+++ database/LogDB/Structure/20250113_r_user_type_tambah_Others.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 10.4.32-MariaDB : Database - phoenixfixed
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `r_user_type` */
+
+DROP TABLE IF EXISTS `r_user_type`;
+
+CREATE TABLE `r_user_type` (
+  `user_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type_name` varchar(191) NOT NULL,
+  PRIMARY KEY (`user_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `r_user_type` */
+
+insert  into `r_user_type`(`user_type_id`,`user_type_name`) values (1,'System Administrator'),(2,'Internal'),(3,'Eksternal'),(4,'Others');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
